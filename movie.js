f2284f8986f3ea94bd6030b98c11cc06123ec257
Ryan Thomas <rythomas2525@gmail.com>
2020-01-07 20:33:43 -0700

    added js files and sources

---
+++ movie.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+
+    function searchMovie(movie) {
+
+
+        var movie = "Space Jam"
+
+        var queryURL = "http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy&"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            console.log(response.Title)
+            $(".has-background-info").text("Movie Title: " + response.Title)
+
+        })
+
+
+    }
+    searchMovie();
+})
