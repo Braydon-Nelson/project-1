786282792647a89d359951b3a2c077b4f093c155
Ryan Thomas <rythomas2525@gmail.com>
2020-01-09 20:22:50 -0700

    messing with dropdown

--- movie.js
+++ movie.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
 
 
-    function searchMovie(movie) {
+    function searchMovie(movieInput) {
 
 
-        var movie = "up"
 
-        var queryURL = "http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy&"
+
+        var queryURL = "http://www.omdbapi.com/?t=" + movieInput + "&apikey=trilogy&"
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
 
-            console.log(response.Title)
+
             $("#movieTitle").text(response.Title)
             $("#movieYear").text("Release Year: " + response.Year)
             $("#movieRated").text("Rated: " + response.Rated)
@@ -26,5 +26,61 @@ $(document).ready(function () {
 
 
     }
-    searchMovie();
+
+    var searchArray = []
+
+    $(".movieSearch").click(function (event) {
+        // Preventing the button from trying to submit the form
+        event.preventDefault();
+        // Storing the city
+        var movieInput = $(".movieSearch").prev().val().trim();
+
+        console.log(movieInput)
+
+        var searchItem = $(this).prev().val()
+        searchArray.push(searchItem)
+
+        localStorage.setItem("search Item", searchArray)
+
+
+        console.log(searchArray)
+
+        var option = document.createElement("option");
+
+        $(option).attr("class", "option1")
+        // $(button).attr("type", "button")
+        option.innerHTML = movieInput
+        $(".formInput").append(option)
+
+        searchMovie(movieInput)
+    });
+
+    function refreshSave() {
+
+        var searchItem = $(this).prev().val()
+
+        // grabbing string out of local storage
+        var item = localStorage.getItem("search Item", searchItem)
+
+        console.log(item)
+        // turning the string of cities into an array
+        var ar = item.split(',');
+
+
+
+        // grabbing the last city searched out of an array no matter how big of array
+        var movieInput = ar.slice(-1)[0]
+
+        console.log(movieInput);
+        searchMovie(movieInput)
+    }
+
+    $(".option1").on("click", function () {
+        movieInput = $(".option1").val()
+        console.log(movieInput)
+
+
+    })
+
+    refreshSave();
 })
